0cbf8ea422350df03f81aa0dcaf3a0dd66e6b850
Nishantdas77 <dasnishant7777@gmail.com>
2025-02-21 20:37:28 +0530

    sign up backend and database successfully completed

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+// Define the User schema
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    fullName: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength:6,
+    },
+    profilePic: {
+        type: String,
+        default: ''
+    }
+},
+{timestamps: true});
+
+const User = mongoose.model('User', userSchema);
+export default User;
