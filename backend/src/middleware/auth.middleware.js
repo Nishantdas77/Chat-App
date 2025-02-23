a6013ddd6f2ab7f54107a20e27e844646caf88eb
Nishantdas77 <dasnishant7777@gmail.com>
2025-02-23 16:40:36 +0530

    Sign Up, Login Page added and connected to backend

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -1,25 +1,30 @@
-import jwt  from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 import User from "../models/user.model.js";
 
-export const proctedRoute = async(req,res)=>{
-    try{
-        const token = req.cookie.jwt;
-        if(!token){
-            return res.status(401).json({message:"Unauthorized -no token provided"})
-        }
-
-        const decoded = jwt.verify(token,process.env.JWT_SECRET)
-        if(!decoded){
-            return res.status(401).json({message:"Unauthorized -invalid token "})
-        }
-        const user = await User.findById(decoded.userId).select("-password")
-        if(!user){
-            return res.status(404).json({message:"User not found"})
-        }
-        req.user=user
-        next()
-    }catch(error){
-        console.log("Error in protected route middleware", error.message)
-        res.status(500).json({message:"Internal Server error"})
+export const proctedRoute = async (req, res, next) => {
+  try {
+    console.log(req.cookies);
+    const token = req.cookies.jwt;
+    if (!token) {
+      return res.status(401).json({ message: "Unauthorized - no token provided" });
     }
-}
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    if (!decoded) {
+      return res.status(401).json({ message: "Unauthorized - invalid token" });
+    }
+
+    console.log(decoded);
+    const user = await User.findById(decoded.UserId).select("-password");
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    req.user = user;
+    
+    // Call the next middleware function in the chain
+    next();
+  } catch (error) {
+    console.log("Error in protected route middleware", error.message);
+    res.status(500).json({ message: "Internal Server error" });
+  }
+};
